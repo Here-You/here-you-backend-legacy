06ed07585794b837661e4af7bd313ffd0493c3b5
kaaang <kaaang@local>
2024-01-16 20:28:45 +0900

    feat: Add login API

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Post('/login')
+  Login(@Body('email') email: string, @Body('password') password: string) {
+    return this.userService.Login(email, password);
+  }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
+import bcrypt from 'bcrypt';
+import jsonwebtoken from 'jsonwebtoken';
+import { UserEntity } from './user.entity';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  private _hashPassword(password: string): string {
+    return bcrypt.hashSync(password, 10);
+  }
+
+  private _comparePassword(password: string, hash: string): boolean {
+    return bcrypt.compareSync(password, hash);
+  }
+
+  private _generateToken(payload: Record<string, unknown>) {
+    return jsonwebtoken.sign(payload, process.env.JWT_SECRET, {
+      expiresIn: '1d',
+    });
+  }
+
+  async Login(email: string, password: string) {
+    const user = await UserEntity.findOne({
+      where: {
+        email: email.toString() ?? '',
+      },
+    });
+
+    if (!user) {
+      throw new HttpException('Invalid credentials', 403);
+    }
+
+    if (!this._comparePassword(password, user.password)) {
+      throw new HttpException('Invalid credentials', 403);
+    }
+
+    return {
+      success: true,
+      token: this._generateToken({
+        id: user.id,
+      }),
+    };
+  }
+}
