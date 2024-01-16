425faa794ea956fd2acb95340ddde2129a279f74
kaaang <kaaang@local>
2024-01-16 15:53:02 +0900

    chore: Add database connection and config

---
+++ src/database/database.providers.ts
@@ -0,0 +1,21 @@
+import { DataSource } from 'typeorm';
+
+export const databaseProviders = [
+  {
+    provide: 'DATA_SOURCE',
+    useFactory: async () => {
+      const dataSource = new DataSource({
+        type: 'mysql',
+        host: process.env.DB_HOST,
+        port: parseInt(process.env.DB_PORT, 10),
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+        synchronize: process.env.DB_SYNC.toString() === 'true',
+      });
+
+      return dataSource.initialize();
+    },
+  },
+];
