5ad6f28a04171234131760a8680b864775d87200
You Jeong Jang <sunny819819@naver.com>
2024-01-24 00:33:14 +0900

    [Bug Fix] eslint prettier 에러 해결
    #6 개발 중 발생한 버그 FIX

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,4 +22,19 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
   },
+  'prettier/prettier': [
+    'error',
+    {
+      arrowSpacing: ['error', { before: true, after: true }],
+      singleQuote: true,
+      semi: false,
+      useTabs: false,
+      tabWidth: 2,
+      trailingComma: 'none',
+      printWidth: 80,
+      bracketSpacing: true,
+      arrowParens: 'always',
+      endOfLine: 'auto', // 이 부분이 lf로 되어있다면 auto로 변경
+    },
+  ],
 };
