8f2af3f25f4508cce66a0d82581f4da091371033
moonyaeyoon <moonyaeyoon@naver.com>
2024-02-03 03:14:10 +0900

    feat : S3 연결

---
+++ aws-s3/aws-s3.service.ts
@@ -0,0 +1,30 @@
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+import { Inject, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { GetDiaryImgUrlDto } from 'src/diary/dtos/get-diary-img-url.dto';
+
+@Injectable()
+export class AwsS3Service {
+  constructor(
+    @Inject('S3_CLIENT')
+    private readonly s3Client: S3Client,
+
+    private readonly configService: ConfigService,
+  ) {}
+  async getDiaryImgUrl(getDiaryImgUrlDto: GetDiaryImgUrlDto, diaryId: number) {
+    // 저장될 파일 이름
+    const fileName = `profile/${diaryId}/${Date.now()}${
+      getDiaryImgUrlDto.fileName
+    }`;
+
+    // Put. 즉, s3에 데이터를 집어넣는 작업에 대한 url 생성
+    const command = new PutObjectCommand({
+      Bucket: this.configService.get('S3_BUCKET'),
+      Key: fileName,
+    });
+
+    const postedImgUrl = await getSignedUrl(this.s3Client, command);
+    return postedImgUrl;
+  }
+}
--- src/diary/dtos/create-diary.dto.ts
+++ src/diary/dtos/create-diary.dto.ts
@@ -1,12 +1,21 @@
-import { IsString, IsDateString, IsEnum } from 'class-validator';
-import { PickType } from '@nestjs/swagger';
-import { DiaryEntity } from '../models/diary.entity';
+import { IsString, IsOptional, IsEnum } from 'class-validator';
 
-export class CreateDiaryDto extends PickType(DiaryEntity, [
-  'title',
-  'place',
-  'weather',
-  'mood',
-  'content',
-  'image',
-]) {}
+export class CreateDiaryDto {
+  @IsString()
+  title: string;
+
+  @IsString()
+  place: string;
+
+  @IsEnum(['CLOUDY', 'RAINY', 'SNOWY', 'PARTLY_CLOUDY', 'SUNNY'])
+  weather: 'CLOUDY' | 'RAINY' | 'SNOWY' | 'PARTLY_CLOUDY' | 'SUNNY';
+
+  @IsEnum(['ANGRY', 'SAD', 'SMILE', 'HAPPY', 'SHOCKED'])
+  mood: 'ANGRY' | 'SAD' | 'SMILE' | 'HAPPY' | 'SHOCKED';
+
+  @IsString()
+  content: string;
+
+  @IsString()
+  image: string;
+}
