dea0420199036ea5cfa0f3ec98b34b5857ff2e91
kaaang <kaaang@local>
2024-02-08 02:02:17 +0900

    chore: max body size를 50MB로 상향하라

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { urlencoded, json } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Todo: Enable CORS only for development, specify the origin in production
   app.enableCors();
   app.setGlobalPrefix('api/v1');
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ extended: true, limit: '50mb' }));
   await app.listen(3000);
 }
 bootstrap();
