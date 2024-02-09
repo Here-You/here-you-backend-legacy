4a1d3df7f021369ff3715b84ec193a82f2bd5ab4
You Jung <80906691+JangYouJung@users.noreply.github.com>
2024-02-10 00:49:33 +0900

    fix: 메이트 탐색 - 현재 로그인한 사용자 닉네임 추가

--- src/mate/dto/mate-recommend-profile.dto.ts
+++ src/mate/dto/mate-recommend-profile.dto.ts
@@ -4,8 +4,8 @@ import { MateSignatureCoverDto } from './mate-signature-cover.dto';
 
 export class MateRecommendProfileDto{
   _id: number;
-  userImage: string;                       // 유저 사진
-  userName: string;                        // 유저 별명
+  mateImage: string;                       // 유저 사진
+  mateName: string;                        // 유저 별명
   is_followed: boolean;                    // 팔로우 여부
   introduction: string;                    // 한 줄 소개
   signatures: MateSignatureCoverDto[];
--- src/mate/dto/mate-with-common-location-response.dto.ts
+++ src/mate/dto/mate-with-common-location-response.dto.ts
@@ -4,5 +4,6 @@ import { MateRecommendProfileDto } from './mate-recommend-profile.dto';
 
 export class MateWithCommonLocationResponseDto{
   location: string;
+  userName: string;   // #112 로그인한 사용자 닉네임 추가
   mateProfiles: MateRecommendProfileDto[];
 }
