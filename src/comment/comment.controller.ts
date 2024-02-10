7b4b629aaab511e766a744c4c104df734b515cae
yewonahn <yewonahn@naver.com>
2024-02-11 02:56:22 +0900

    feat : 댓글 수정, 삭제 기능 구현 완료
    로컬 테스트 완료

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -1,10 +1,12 @@
-import {Controller, Post, Body, Req, UseGuards, Param, Patch} from '@nestjs/common';
+import {Controller, Post, Body, Req, UseGuards, Param, Patch, Delete} from '@nestjs/common';
 import { CommentService } from './comment.service';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { ResponseCode } from '../response/response-code.enum';
 import { ResponseDto } from '../response/response.dto';
 import { UserGuard } from '../user/user.guard';
 import { Request } from 'express';
+import {UserEntity} from "../user/user.entity";
+import {RuleMainEntity} from "../rule/domain/rule.main.entity";
 
 @Controller('mate/rule')
 export class CommentController {
@@ -36,26 +38,42 @@ export class CommentController {
   }
 
   // 여행 규칙 코멘트 수정
-  /*
-  @Patch('/:ruleId')
+  @Patch('/:ruleId/:commentId')
   @UseGuards(UserGuard)
-  async updateComment(@Body() dto: CreateCommentDto, @Param('ruleId') ruleId: number, @Req() req: Request): Promise<ResponseDto<any>> {
-    const result = await this.commentService.updateComment(dto, ruleId, req.user.id);
-
-    if(!result){
-      return new ResponseDto(
-          ResponseCode.COMMENT_UPDATE_FAIL,
-          false,
-          "여행 규칙 코멘트 수정 실패",
-          null);
-    }
-    else{
+  async updateComment(@Body() dto: CreateCommentDto, @Param('ruleId') ruleId: number, @Param('commentId') commentId: number,@Req() req: Request): Promise<ResponseDto<any>> {
+    try {
+      const result = await this.commentService.updateComment(dto, ruleId, req.user.id, commentId);
       return new ResponseDto(
           ResponseCode.COMMENT_UPDATE_SUCCESS,
           true,
           "여행 규칙 코멘트 수정 성공",
           result);
+    } catch (e) {
+      return new ResponseDto(
+          ResponseCode.COMMENT_UPDATE_FAIL,
+          false,
+          e.message,
+          null);
     }
   }
-   */
+
+  // 여행 규칙 코멘트 삭제
+  @Delete('/:ruleId/:commentId')
+  @UseGuards(UserGuard)
+  async deleteComment(@Param('ruleId') ruleId: number, @Param('commentId') commentId: number,@Req() req: Request): Promise<ResponseDto<any>> {
+    try {
+      const result = await this.commentService.deleteComment(ruleId, req.user.id, commentId);
+      return new ResponseDto(
+          ResponseCode.COMMENT_DELETE_SUCCESS,
+          true,
+          "여행 규칙 코멘트 삭제 성공",
+          result);
+    } catch (e) {
+      return new ResponseDto(
+          ResponseCode.COMMENT_DELETE_FAIL,
+          false,
+          e.message,
+          null);
+    }
+  }
 }
--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -28,9 +28,54 @@ export class CommentService {
     return comment.id;
   }
 
-  /*
-  async updateComment(dto: CreateCommentDto, ruleId: number, userId: number) : Promise<number> {
+  async updateComment(dto: CreateCommentDto, ruleId: number, userId: number, commentId: number) : Promise<number> {
+    try {
+      // 사용자, 규칙, 댓글 검증
+      const user = await UserEntity.findExistUser(userId);
+      if (!user) throw new NotFoundException('사용자를 찾을 수 없습니다');
+      const rule = await RuleMainEntity.findRuleById(ruleId);
+      if (!rule) throw new NotFoundException('존재하지 않는 규칙입니다');
 
+      const comment = await CommentEntity.findOne({
+        where: {user: {id: userId}, rule: {id: ruleId}}
+      })
+      if(!comment) throw new NotFoundException("데이터를 찾을 수 없습니다");
+
+      if(comment.id != commentId) throw new NotFoundException('해당 댓글 수정 권한이 없는 사용자입니다');
+
+      if (comment.id == commentId) {
+        comment.content = dto.content;
+        await CommentEntity.save(comment);
+        return comment.id;
+      }
+    } catch (e) {
+      console.log('해당 댓글 수정 권한이 없는 사용자 입니다');
+      throw new Error(e.message);
+    }
   }
-  */
+
+  async deleteComment(ruleId: number, userId: number, commentId: number) : Promise<number> {
+    try {
+      // 사용자, 규칙, 댓글 검증
+      const user = await UserEntity.findExistUser(userId);
+      if (!user) throw new NotFoundException('사용자를 찾을 수 없습니다');
+      const rule = await RuleMainEntity.findRuleById(ruleId);
+      if (!rule) throw new NotFoundException('존재하지 않는 규칙입니다');
+
+      const comment = await CommentEntity.findOne({
+        where: {user: {id: userId}, rule: {id: ruleId}}
+      })
+      if (!comment) throw new NotFoundException("데이터를 찾을 수 없습니다");
+
+      if (comment.id != commentId) throw new NotFoundException('해당 댓글을 작성한 사용자가 아닙니다');
+
+      if (comment.id == commentId) {
+        await comment.softRemove();
+        return comment.id;
+      }
+    } catch (e) {
+      console.log('해당 댓글 삭제 권한이 없는 사용자 입니다');
+      throw new Error(e.message);
+    }
+  }
 }
