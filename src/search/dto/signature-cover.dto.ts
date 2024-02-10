2d21d12e6838eb39fc7664b2f4ec225b097e3fae
You Jung <80906691+JangYouJung@users.noreply.github.com>
2024-02-11 02:58:34 +0900

    Refactor: 시그니처 dto 및 response code 코드 수정

--- src/search/dto/signature-cover.dto.ts
+++ src/search/dto/signature-cover.dto.ts
@@ -1,6 +1,6 @@
-// cover-signature.dto.ts
+// signature-cover.dto.ts
 
-export class CoverSignatureDto {
+export class SignatureCoverDto {
   _id: number;
   title: string;
   image: string;      // 시그니처 첫 번째 페이지 사진
