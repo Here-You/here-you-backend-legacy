9b8cc638151175bd34e1bd465e2482e57a17099b
yewonahn <yewonahn@naver.com>
2024-02-12 02:56:29 +0900

    fix : controller 수정

--- src/rule/rule.controller.ts
+++ src/rule/rule.controller.ts
@@ -15,29 +15,8 @@ export class RuleController {
     private readonly ruleService: RuleService,
   ) {}
 
-  // [1] 여행 규칙 멤버 리스트 조회
-  @Get('member/:ruleId')
-  async getMemberList(@Param('ruleId') ruleId : number) : Promise<ResponseDto<any>> {
-    try {
-      const memberList = await this.ruleService.getMemberList(ruleId);
-      return new ResponseDto(
-          ResponseCode.GET_MEMBER_LIST_SUCCESS,
-          true,
-          "여행 규칙 멤버 리스트 불러오기 성공",
-          memberList
-      );
-    } catch (error) {
-      return new ResponseDto(
-          ResponseCode.GET_MEMBER_LIST_FAIL,
-          false,
-          "여행 규칙 멤버 리스트 불러오기 실패",
-          null
-      );
-    }
-  }
-
-  // [2] 여행 규칙 상세 페이지 조회 (댓글) - 페이지네이션
-  @Get('/detail/:ruleId')
+  // [1] 여행 규칙 상세 페이지 조회 (댓글) - 페이지네이션
+  @Get('/detail/comment/:ruleId')
   @UseGuards(UserGuard)
   async getComment(@Req() req: Request,
                    @Param('ruleId') ruleId: number,
@@ -62,7 +41,53 @@ export class RuleController {
     }
   }
 
-  // [3] 여행 규칙 상세 페이지 조회 (게시글)
+  // [2] 여행 규칙 멤버 리스트 조회
+  @Get('/detail/member/:ruleId')
+  async getMemberList(@Param('ruleId') ruleId : number) : Promise<ResponseDto<any>> {
+    try {
+      const memberList = await this.ruleService.getMemberList(ruleId);
+      return new ResponseDto(
+          ResponseCode.GET_MEMBER_LIST_SUCCESS,
+          true,
+          "여행 규칙 멤버 리스트 불러오기 성공",
+          memberList
+      );
+    } catch (error) {
+      return new ResponseDto(
+          ResponseCode.GET_MEMBER_LIST_FAIL,
+          false,
+          "여행 규칙 멤버 리스트 불러오기 실패",
+          null
+      );
+    }
+  }
+
+  // [3] 여행 규칙 참여 멤버로 초대할 메이트 검색 결과
+  @Get('/detail/search/:ruleId')
+  @UseGuards(UserGuard)
+  async getSearchMember(
+      @Query('searchTerm')searchTerm : string,
+      @Param('ruleId') ruleId: number,
+      @Req() req: Request): Promise<ResponseDto<any>> {
+    try {
+      const getSearchMemberDto : GetSearchMemberDto[] = await this.ruleService.getSearchMember(req.user.id, ruleId, searchTerm)
+      return new ResponseDto(
+          ResponseCode.GET_SEARCH_RESULT_SUCCESS,
+          true,
+          "초대할 메이트 검색 결과 리스트 불러오기 성공",
+          getSearchMemberDto
+      );
+    } catch (error) {
+      return new ResponseDto(
+          ResponseCode.GET_SEARCH_RESULT_FAIL,
+          false,
+          "초대할 메이트 검색 결과 리스트 불러오기 실패",
+          null
+      );
+    }
+  }
+
+  // [4] 여행 규칙 상세 페이지 조회 (게시글)
   @Get('/detail/:ruleId')
   @UseGuards(UserGuard)
   async getDetail(@Req() req: Request, @Param('ruleId') ruleId: number): Promise<ResponseDto<any>> {
@@ -87,7 +112,7 @@ export class RuleController {
     }
   }
 
-  // [4] 여행 규칙 수정
+  // [5] 여행 규칙 수정
   @Patch('/detail/:ruleId')
   @UseGuards(UserGuard)
   async updateRule(@Body() updateRuleDto: UpdateRuleDto, @Req() req: Request, @Param('ruleId') ruleId: number): Promise<ResponseDto<any>> {
@@ -112,79 +137,30 @@ export class RuleController {
     }
   }
 
-  // [5] 여행 규칙 전체 리스트 조회
-  @Get('list')
-  @UseGuards(UserGuard)
-  async getRuleList(@Req() req: Request): Promise<ResponseDto<any>> {
-    const result = await this.ruleService.getRuleList(req.user.id);
-
-    if(!result){
-      return new ResponseDto(
-          ResponseCode.GET_RULE_LIST_FAIL,
-          false,
-          "여행 규칙 전체 리스트 조회 실패",
-          null);
-
-    }
-    else{
-      return new ResponseDto(
-          ResponseCode.GET_RULE_LIST_SUCCESS,
-          true,
-          "여행 규칙 전체 리스트 조회 성공",
-          result);
-    }
-  }
-
   // [6] 여행 규칙 생성
-  @Post('/write')
+  @Post('/detail')
   @UseGuards(UserGuard)
   async createRule(@Req() req: Request, @Body() createRuleDto: CreateRuleDto): Promise<ResponseDto<any>> {
     const result = await this.ruleService.createRule(createRuleDto, req.user.id);
 
     if(!result){
       return new ResponseDto(
-        ResponseCode.RULE_CREATION_FAIL,
-        false,
-        "여행 규칙 생성 실패",
-        null);
+          ResponseCode.RULE_CREATION_FAIL,
+          false,
+          "여행 규칙 생성 실패",
+          null);
 
     }
     else{
       return new ResponseDto(
-        ResponseCode.RULE_CREATED,
-        true,
-        "여행 규칙 생성 성공",
-        result);
-    }
-  }
-
-  // [7] 여행 규칙 참여 멤버로 초대할 메이트 검색 결과
-  @Get('/write/search/:ruleId')
-  @UseGuards(UserGuard)
-  async getSearchMember(
-      @Query('searchTerm')searchTerm : string,
-      @Param('ruleId') ruleId: number,
-      @Req() req: Request): Promise<ResponseDto<any>> {
-    try {
-      const getSearchMemberDto : GetSearchMemberDto[] = await this.ruleService.getSearchMember(req.user.id, ruleId, searchTerm)
-      return new ResponseDto(
-          ResponseCode.GET_SEARCH_RESULT_SUCCESS,
+          ResponseCode.RULE_CREATED,
           true,
-          "초대할 메이트 검색 결과 리스트 불러오기 성공",
-          getSearchMemberDto
-      );
-    } catch (error) {
-      return new ResponseDto(
-          ResponseCode.GET_SEARCH_RESULT_FAIL,
-          false,
-          "초대할 메이트 검색 결과 리스트 불러오기 실패",
-          null
-      );
+          "여행 규칙 생성 성공",
+          result);
     }
   }
 
-
-  // 여행 규칙 나가기
+  // [7] 여행 규칙 나가기
   /*
   @Delete('/:ruleId')
   @UseGuards(UserGuard)
@@ -212,4 +188,27 @@ export class RuleController {
     }
   }
    */
+
+  // [8] 여행 규칙 전체 리스트 조회
+  @Get()
+  @UseGuards(UserGuard)
+  async getRuleList(@Req() req: Request): Promise<ResponseDto<any>> {
+    const result = await this.ruleService.getRuleList(req.user.id);
+
+    if(!result){
+      return new ResponseDto(
+          ResponseCode.GET_RULE_LIST_FAIL,
+          false,
+          "여행 규칙 전체 리스트 조회 실패",
+          null);
+
+    }
+    else{
+      return new ResponseDto(
+          ResponseCode.GET_RULE_LIST_SUCCESS,
+          true,
+          "여행 규칙 전체 리스트 조회 성공",
+          result);
+    }
+  }
 }
