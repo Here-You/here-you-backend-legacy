872c9a738833ab1738af584d98a5c323aba79daa
kaaang <kaaang@local>
2024-02-12 00:42:22 +0900

    fix: S3 Util Service를 user module에 추가하라

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { S3UtilService } from '../utils/S3.service';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, S3UtilService],
 })
 export class UserModule {}
