bc135c9435f31339eb655956937446206966d20c
You Jung <80906691+JangYouJung@users.noreply.github.com>
2024-02-13 03:47:01 +0900

    refactor: 시그니처 dto 파일 경로 리팩토링

--- src/signature/signature.comment.controller.ts
+++ src/signature/signature.comment.controller.ts
@@ -1,11 +1,13 @@
 // signature.comment.controller.ts
 
-import { Body, Controller, Param, Post, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query, Req, UseGuards } from '@nestjs/common';
 import { SignatureCommentService } from './signature.comment.service';
 import { UserGuard } from '../user/user.guard';
 import { Request } from 'express';
-import { CreateSignatureDto } from './dto/create-signature.dto';
-import { CreateCommentDto } from './dto/create-comment.dto';
+import { CreateCommentDto } from './dto/comment/create-comment.dto';
+import { ResponseDto } from '../response/response.dto';
+import { ResponseCode } from '../response/response-code.enum';
+import { CursorPageOptionsDto } from '../rule/dto/cursor-page.options.dto';
 
 @Controller('signature/:signatureId')
 export class SignatureCommentController{
@@ -13,7 +15,7 @@ export class SignatureCommentController{
 
   @Post('/comment')
   @UseGuards(UserGuard)
-  async createSignatureComment(
+  async createSignatureComment( // 시그니처 댓글 생성하기
     @Req() req: Request,
     @Param('signatureId') signatureId: number,
     @Body() newComment: CreateCommentDto,
@@ -21,29 +23,84 @@ export class SignatureCommentController{
     try{
       const result = await this.signatureCommentService.createSignatureComment(newComment, req.user.id, signatureId)
 
+      return new ResponseDto(
+        ResponseCode.CREATE_SIGNATURE_COMMENT_SUCCESS,
+        true,
+        "시그니처 댓글 생성 성공",
+        result
+      );
+
     }
     catch(error){
       console.log('Error on createSigComment: ',error);
-
+      return new ResponseDto(
+        ResponseCode.COMMENT_CREATION_FAIL,
+        false,
+        "시그니처 댓글 생성 실패",
+        null
+      );
     }
   }
 
   @Post('/comment/:commentId')
   @UseGuards(UserGuard)
-  async createSignatureReplyComment(
+  async createSignatureReplyComment(  // 시그니처 답글 생성하기
     @Req() req: Request,
     @Param('signatureId') signatureId: number,
     @Param('commentId') commentId: number,
     @Body() newComment: CreateCommentDto,
   ){
     try{
+      const result = await this.signatureCommentService.createSignatureComment(newComment, req.user.id, signatureId, commentId)
+
+      return new ResponseDto(
+        ResponseCode.CREATE_SIGNATURE_COMMENT_SUCCESS,
+        true,
+        "시그니처 답글 생성 성공",
+        result
+      );
 
     }
     catch(error){
-      console.log('Error on createSigChildComment: ',error);
-
+      console.log('Error on createSigComment: ',error);
+      return new ResponseDto(
+        ResponseCode.COMMENT_CREATION_FAIL,
+        false,
+        "시그니처 답글 생성 실패",
+        null
+      );
     }
   }
+
+  @Get('/comment')
+  @UseGuards(UserGuard)
+  async getSignatureComment(  // 시그니처 댓글 조회하기 (무한 스크롤)
+    @Req() req: Request,
+    @Param('signatureId') signatureId: number,
+    @Query() cursorPageOptionsDto: CursorPageOptionsDto,
+  ){
+    try{
+      const result = await this.signatureCommentService.getSignatureComment(cursorPageOptionsDto, req.user.id, signatureId);
+
+      return new ResponseDto(
+        ResponseCode.GET_COMMENT_DETAIL_SUCCESS,
+        true,
+        "시그니처 댓글 가져오기 성공",
+        result
+      );
+    }
+    catch(error){
+      console.log('Error on createSigChildComment: ',error);
+      return new ResponseDto(
+        ResponseCode.GET_COMMENT_DETAIL_FAIL,
+        false,
+        "시그니처 댓글 가져오기 실패",
+        null
+      );
+    }
+  }
+
+
 
 
 
