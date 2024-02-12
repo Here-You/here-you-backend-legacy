29b18bd903e3b81d3586dade2ded4005983bd5a4
yewonahn <yewonahn@naver.com>
2024-02-12 18:06:42 +0900

    fix : 댓글 수정, 삭제 권한 검증 수정

--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -33,25 +33,31 @@ export class CommentService {
   async updateComment(dto: CreateCommentDto, ruleId: number, userId: number, commentId: number) : Promise<number> {
     try {
       // 사용자, 규칙, 댓글 검증
-      const user = await UserEntity.findExistUser(userId);
+      const user = await UserEntity.findOne({
+        where: {id : userId},
+      });
       if (!user) throw new NotFoundException('사용자를 찾을 수 없습니다');
-      const rule = await RuleMainEntity.findRuleById(ruleId);
-      if (!rule) throw new NotFoundException('존재하지 않는 규칙입니다');
-
-      const comment = await CommentEntity.findOne({
-        where: {user: {id: userId}, rule: {id: ruleId}}
+      const rule = await RuleMainEntity.findOne({
+        where: {id: ruleId},
       })
-      if(!comment) throw new NotFoundException("데이터를 찾을 수 없습니다");
+      if (!rule) throw new NotFoundException('존재하지 않는 규칙입니다');
+      const comment = await CommentEntity.findOne({
+        where: {id: commentId}
+      });
+      if (!comment) throw new NotFoundException('존재하지 않는 댓글 입니다');
 
-      if(comment.id != commentId) throw new NotFoundException('해당 댓글 수정 권한이 없는 사용자입니다');
-
-      if (comment.id == commentId) {
+      const checkValidateUser = await CommentEntity.findOne({
+        where: {id: commentId, user: {id: userId}, rule: {id: ruleId}}}
+      )
+      if (!!checkValidateUser) {
         comment.content = dto.content;
         await CommentEntity.save(comment);
         return comment.id;
+      } else {
+        throw new NotFoundException('해당 댓글 수정 권한이 없는 사용자 입니다');
       }
     } catch (e) {
-      console.log('해당 댓글 수정 권한이 없는 사용자 입니다');
+      console.log('검증 과정에서 에러 발생');
       throw new Error(e.message);
     }
   }
@@ -64,23 +70,28 @@ export class CommentService {
         where: {id : userId},
       });
       if (!user) throw new NotFoundException('사용자를 찾을 수 없습니다');
-
       const rule = await RuleMainEntity.findOne({
         where: {id: ruleId},
       })
       if (!rule) throw new NotFoundException('존재하지 않는 규칙입니다');
+      const comment = await CommentEntity.findOne({
+        where: {id: commentId}
+      });
+      if (!comment) throw new NotFoundException('존재하지 않는 댓글 입니다');
 
       // 해당 규칙에, 해당 사용자가 작성한, 해당 댓글 ID를 가진 댓글이 있는지 검증
-      const comment = await CommentEntity.findOne({
-        where: {id: commentId, user:{id: userId}, rule:{id: ruleId}}
-      })
-      if (!!comment) {
+      const checkValidateUser = await CommentEntity.findOne({
+        where: {id: commentId, user: {id: userId}, rule: {id: ruleId}}}
+      )
+      if (!!checkValidateUser) {
         await comment.softRemove();
         console.log('댓글 삭제 성공');
         return comment.id;
+      } else {
+        throw new NotFoundException('해당 댓글 삭제 권한이 없는 사용자 입니다');
       }
     } catch (e) {
-      console.log('해당 댓글 삭제 권한이 없는 사용자 입니다');
+      console.log('검증 과정에서 에러 발생');
       throw new Error(e.message);
     }
   }
