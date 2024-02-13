a2e47ba130056e97e1baf67f6a7bea6dbd2ea2d5
You Jung <80906691+JangYouJung@users.noreply.github.com>
2024-02-13 18:51:28 +0900

    [Refactor] 댓글 날짜 반환값 타입 수정

--- src/signature/dto/comment/get-signature-comment.dto.ts
+++ src/signature/dto/comment/get-signature-comment.dto.ts
@@ -7,6 +7,6 @@ export class GetSignatureCommentDto{
   parentId: number;
   content: string;
   writer: GetCommentWriterDto;
-  date: string;        // 생성 | 수정일
+  date: Date;         // 생성 | 수정일
   is_edited: boolean; // 댓글 수정 여부
 }
